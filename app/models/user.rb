97d9356404d6774b268a109f842eab17412390a9
Andrei Navarro <dreinavarro@gmail.com>
2018-06-11 16:10:39 +0800

    implemented validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates :email, :first_name, :last_name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should have an email" do
+    user = User.new(email: nil, first_name: "something", last_name: "something")
+    expect(user.save).to eq(false)
+  end
+
+  it "should have a first name" do
+    user = User.new(email: "something@example.com", first_name: nil, last_name: "something")
+    expect(user.save).to eq(false)
+  end
+
+  it "should have a last name" do
+    user = User.new(email: "something@example.com", first_name: "something", last_name: nil)
+    expect(user.save).to eq(false)
+  end
 end
