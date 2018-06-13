2fdc2b1c083572f39b77ddbf64b962469de2a67f
Andrey <ramirez.jag00@gmail.com>
2018-06-13 16:31:06 +0800

    Used should-matchers for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it "should have an email" do
-    user = User.new(email: nil, first_name: "something", last_name: "something")
-    expect(user.save).to eq(false)
-  end
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
 
-  it "should have a first name" do
-    user = User.new(email: "something@example.com", first_name: nil, last_name: "something")
-    expect(user.save).to eq(false)
-  end
-
-  it "should have a last name" do
-    user = User.new(email: "something@example.com", first_name: "something", last_name: nil)
-    expect(user.save).to eq(false)
-  end
 end
