161644ccb06381d75b336b11e120fe00e5247686
Andrey <ramirez.jag00@gmail.com>
2018-06-14 16:17:05 +0800

    fixed model indentations

--- app/models/monthly_reward.rb
+++ app/models/monthly_reward.rb
@@ -1,5 +1,5 @@
 class MonthlyReward < ApplicationRecord
-	validates :point, presence: true, numericality: { only_integer: true, other_than: 0 }
-	validates :description, presence: true
+  validates :point, presence: true, numericality: { only_integer: true, other_than: 0 }
+  validates :description, presence: true
   belongs_to :departments
 end
