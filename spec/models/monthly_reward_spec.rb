51579c501ba985009f14816e0608cf6fdd743e25
Andrey <ramirez.jag00@gmail.com>
2018-06-14 11:15:35 +0800

    Used be_valid and be_invalid for spec of vote and monthly_reward model

--- spec/models/monthly_reward_spec.rb
+++ spec/models/monthly_reward_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe MonthlyReward, type: :model do
-  # it "should have description" do
-  #   monthly_reward = MonthlyReward.new(point: 1, description: nil)
-  #   expect(monthly_reward.valid?).to eq(false)
-  # end
 
   it { should validate_presence_of(:point) }
   
-  it "should have a point and must be other than 0" do
-    monthly_reward = MonthlyReward.new(point: 0, description: "something")
-    expect(monthly_reward.save).to eq(false)
+  it "should have an integer point where 0 is invalid" do
+    monthly_reward = MonthlyReward.create(point: 0, description: "something")
+    expect(monthly_reward).to be_invalid
+  end
+
+  it "should have an integer point where it is less than 0" do
+    monthly_reward = MonthlyReward.create(point: -1, description: "something")
+    expect(monthly_reward).to be_valid
+  end
+
+  it "should have an integer point where it is greater than 0" do
+    monthly_reward = MonthlyReward.create(point: 1, description: "something")
+    expect(monthly_reward).to be_valid
   end
 
   it { should validate_presence_of(:description) }
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
+
   it { should validate_presence_of(:point) }
   
-  it "should have a point and must be other than 0" do
-    monthly_reward = MonthlyReward.new(point: 0, description: "something")
-    expect(monthly_reward.save).to eq(false)
+  it "should have an integer point where 0 is invalid" do
+    monthly_reward = MonthlyReward.create(point: 0, description: "something")
+    expect(monthly_reward).to be_invalid
+  end
+
+  it "should have an integer point where it is less than 0" do
+    monthly_reward = MonthlyReward.create(point: -1, description: "something")
+    expect(monthly_reward).to be_valid
+  end
+
+  it "should have an integer point where it is greater than 0" do
+    monthly_reward = MonthlyReward.create(point: 1, description: "something")
+    expect(monthly_reward).to be_valid
   end
   
   it { should validate_numericality_of(:point).only_integer }
+  
 end
