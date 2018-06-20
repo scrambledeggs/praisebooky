073e713345de37f73c8df8d6f138d16499f9de32
Clyde <clydeang421@gmail.com>
2018-06-20 13:23:03 +0800

    Revert "Comment feature - not yet working"
    This reverts commit 8058b062fffd5d60e8811a3251a027206dc2029f.

--- Gemfile
+++ Gemfile
@@ -65,4 +65,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'jquery-rails'
 gem 'bootstrap', '~> 4.1.1'
-gem 'responders', '~> 2.0'
--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -1,30 +1,4 @@
 class VotersController < ApplicationController
-
-  respond_to :html, :json
-
   def index
   end
-
-
-  def new
-    @vote = Vote.new
-    respond_modal_with @vote
-  end
-
-  def create
-    @vote = Vote.create(vote_params)
-    respond_modal_with @vote, location: votes_path
-  end
-
-  private
-    def set_vote
-      @vote = Vote.find(params[:id])
-    end
-
-    def vote_params
-      params.require(:vote).permit(:comment)
-    end
-  end
-  
-
-
+end
