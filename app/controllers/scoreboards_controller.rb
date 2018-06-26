7fd041331bbabe2706a13638dadcd00426ea97cf
Clyde <clydeang421@gmail.com>
2018-06-26 12:56:44 +0800

    commented out bloat

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,7 +1,7 @@
 class ScoreboardsController < ApplicationController
   def index
     @users = User.all.order(updated_at: :desc)
-    User.joins(:votes).group("users.department_id").sum(:point)
+#    User.joins(:votes).group("users.department_id").sum(:point)
   end
 
   def new
