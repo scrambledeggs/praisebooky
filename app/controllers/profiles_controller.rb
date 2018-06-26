a2d9e77274070f39f386e8bb4e10c2f0b0773a8e
Clyde <clydeang421@gmail.com>
2018-06-26 17:55:49 +0800

    updated vote to have receiver attrib

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,8 +6,8 @@ class ProfilesController < ApplicationController
     @users = User.all
     
     current_user = User.find(10)
-    @remaining_votes_user = 25 - current_user.votes.count
-    @points = Vote.where(receiver_id: current_user.id).all.sum(:point) + 25
+    @remaining_votes_user = 25 - Vote.where(voter: current_user).where("point > ?", 0).all.sum(:point) + Vote.where(voter: current_user).where("point < ?", 0).all.sum(:point)
+    @points = Vote.where(receiver: current_user).all.sum(:point) + 25
   end
 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,6 @@ class Vote < ApplicationRecord
   validates :point, presence: true, numericality: { only_integer: true, other_than: 0 }
 
   belongs_to :department
-  belongs_to :user
+  belongs_to :voter, class_name: "User", foreign_key: "user_id"
+  belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
 end
