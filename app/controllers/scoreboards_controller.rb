44d4b4e6b183efa2c8c5477c21eb0b1d4dc9f1cc
Clyde <clydeang421@gmail.com>
2018-06-26 12:02:54 +0800

    removed bloat

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,11 +1,7 @@
 class ScoreboardsController < ApplicationController
   def index
-  	@users = User.all.order(updated_at: :desc)
- 	User.joins(:votes).group("users.department_id").sum(:point)
-  end
-
-  def sum(*args)
-  	calculate(:sum, *args)
+    @users = User.all.order(updated_at: :desc)
+    User.joins(:votes).group("users.department_id").sum(:point)
   end
 
   def new
