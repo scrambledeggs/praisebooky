0ab26eb6c1d6c2d9900496e15f6a935b7192180b
Clyde <clydeang421@gmail.com>
2018-06-29 14:42:08 +0800

    changed scope of class variables

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,18 @@
 class ProfilesController < ApplicationController
   before_action :set_vote, except: [ :index, :new, :create ]
 
-  t = Time.now
-  @@start_date = t.at_beginning_of_month
-  @@end_date = t.at_end_of_month
-
   def index
+    t = Time.now
+    start_date = t.at_beginning_of_month
+    end_date = t.at_end_of_month
+
     @votes = Vote.all.order(created_at: :desc)
     @users = User.all
     
     current_user = User.find(10)
 
-    @remaining_votes_user = 25 - current_user.votes.where(created_at: @@start_date..@@end_date).count
-    @points = Vote.where(receiver_id: current_user.id).where(created_at: @@start_date..@@end_date).all.sum(:point) + 25
+    @remaining_votes_user = 25 - current_user.votes.where(created_at: start_date..end_date).count
+    @points = Vote.where(receiver_id: current_user.id).where(created_at: start_date..end_date).all.sum(:point) + 25
   end
 
 end
--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,6 +1,5 @@
 class ScoreboardsController < ApplicationController
   def index
     @users = User.all.order(updated_at: :desc)
-    #User.joins(:votes).group("users.department_id").sum(:point)
   end
 end
