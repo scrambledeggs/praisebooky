14fefa850089b065b86a7df9a5b90f162416c366
Clyde <clydeang421@gmail.com>
2018-06-29 14:03:15 +0800

    changed inverse of to foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@
 
   belongs_to :department
 
-  has_many :votes_made, class_name: "Vote", inverse_of: "voter"
+  has_many :votes_made, class_name: "Vote", foreign_key: "user_id"
   validates_associated :votes_made
 
-  has_many :votes_received, class_name: "Vote", inverse_of: "receiver"
+  has_many :votes_received, class_name: "Vote", foreign_key: "receiver_id"
   validates_associated :votes_received
 
   has_many :messages
