d53e7addeb901fdb68bcca9bcf3bef7e52d82a5d
Clyde <clydeang421@gmail.com>
2018-07-03 14:19:20 +0800

    monthly scoreoboard

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,5 +1,9 @@
 class ScoreboardsController < ApplicationController
   def index
+    t = Time.now
+    @start_date = t.at_beginning_of_month
+    @end_date = t.at_end_of_month
+
     @users = User.all.order(updated_at: :desc)
   end
 end
