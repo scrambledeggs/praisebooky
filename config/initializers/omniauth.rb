6ca66fe68cba2628977371f4395b8c3438249e2e
Clyde <clydeang421@gmail.com>
2018-07-04 15:46:44 +0800

    removed bloat from google oauth

--- config/initializers/omniauth.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  #config = Rails.application.config.x.settings["oauth2"]
-  
-  #provider :google_oauth2, config["client_id"],
-                           #config["client_secret"],
-                          # image_size: 150
-
-  provider :google_oauth2, '525808177862-661icl3q2jigqoketbgnhg8n9ds141r4.apps.googleusercontent.com', '0ZB_MfBTPQPMped77U7d8zvP'
-end
