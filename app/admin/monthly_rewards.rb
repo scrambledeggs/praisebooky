2db06a18f096e0c8eb74e933a8b0a448aebba991
Unknown <isabellejavierx@yahoo.com>
2018-07-05 11:46:46 +0800

    Working monthly rewards in scoreboard

--- app/admin/monthly_rewards.rb
+++ app/admin/monthly_rewards.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register MonthlyReward do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :point, :description
+permit_params :point, :description, :department_id, :user_id
 #
 # or
 #
--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,5 +1,6 @@
 class ScoreboardsController < ApplicationController
   def index
     @users = User.order(first_name: :asc)
+    @monthlyrewards = MonthlyReward.all
   end
 end
