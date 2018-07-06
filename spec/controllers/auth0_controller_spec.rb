f6b64fe264a767bc3591c10d6220e1c9396dd1e7
Clyde <clydeang421@gmail.com>
2018-07-06 11:43:56 +0800

    take 2

--- spec/controllers/auth0_controller_spec.rb
+++ spec/controllers/auth0_controller_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Auth0Controller, type: :controller do
   describe "GET #callback" do
     it "returns http success" do
       get :callback
-      expect(response).to have_http_status(:redirection)
+      expect(response).to have_http_status(:redirect)
     end
   end
 
   describe "GET #failure" do
     it "returns http success" do
       get :failure
-      expect(response).to have_http_status(:redirection)
+      expect(response).to have_http_status(:redirect)
     end
   end
 
