ccb165a73bab0684bdb241f69d6ce5d35fb9185c
Clyde <clydeang421@gmail.com>
2018-07-10 14:40:19 +0800

    set_vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,6 @@
 class VotesController < ApplicationController
+  before_action :set_vote, except: [ :index, :new, :create ]
+
   def index
     t = Time.now
     start_date = t.at_beginning_of_month
