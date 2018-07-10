562e288378502c845fc5313514179ad6f676f3da
Clyde <clydeang421@gmail.com>
2018-07-10 16:31:45 +0800

    part 2

--- app/controllers/recent_activities_controller.rb
+++ app/controllers/recent_activities_controller.rb
@@ -1,7 +1,7 @@
 class RecentActivitiesController < ApplicationController
   def index
-    @current_user = User.find(1)
+    @current_user = current_user
     @votes_all = Vote.order(created_at: :desc)
-    @votes_same_department = Vote.where(department: @current_user.department).order(created_at: :desc)
+    @votes_same_department = Vote.where(department: current_user.department).order(created_at: :desc)
   end
 end
