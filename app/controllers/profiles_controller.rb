8062f08c1dd3c48ffb758e8adaa8bf1f27fa27c8
Clyde <clydeang421@gmail.com>
2018-07-10 10:29:12 +0800

    redirects to own profile when clicking other dept

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,14 +2,17 @@ class ProfilesController < ApplicationController
   before_action :set_vote, except: [ :index, :new, :create ]
 
   def index
+    @profile_user = User.find(params[:id])
+    @current_user = User.find(1)
+
+    redirect_to profiles_index_path(id: @current_user) unless @profile_user.department == @current_user.department || @current_user.manager
+
     t = Time.now
     start_date = t.at_beginning_of_month
     end_date = t.at_end_of_month
 
     @votes = Vote.order(created_at: :desc)
-    
-    @profile_user = User.find(params[:id])
-    
+
     @remaining_votes_user = 25 - @profile_user.votes_made.where(created_at: start_date..end_date).where("point > ?", 0).sum(:point) + @profile_user.votes_made.where(created_at: start_date..end_date).where("point < ?", 0).sum(:point)
     @points = @profile_user.votes_received.where(created_at: start_date..end_date).sum(:point) + 25
 
