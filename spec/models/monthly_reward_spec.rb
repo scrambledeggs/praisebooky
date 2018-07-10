d0ec80fbbba2a698700059cd87768bf2b4785f87
Clyde <clydeang421@gmail.com>
2018-07-10 13:02:42 +0800

    monthly reward of same dept only

--- spec/models/monthly_reward_spec.rb
+++ spec/models/monthly_reward_spec.rb
@@ -13,6 +13,5 @@ RSpec.describe MonthlyReward, type: :model do
   it { should validate_presence_of(:description) }
 
   it { should belong_to(:department) }
-  it { should have_many(:user) }
-
+  it { should belong_to(:user) }
 end
